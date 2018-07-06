f4fed3e27c31c5c7306a2f47f07f10fb23774ab8
Miguel Muscat <miguelmuscat93@gmail.com>
2018-07-06 16:50:48 +0200

    Added `TransitionerException`

---
+++ src/Exception/TransitionerException.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Dhii\Data\Exception;
+
+use Dhii\Data\TransitionerInterface;
+use Dhii\Util\String\StringableInterface as Stringable;
+use Exception as RootException;
+
+/**
+ * An exception that is thrown is relation to a transitioner.
+ *
+ * @since [*next-version*]
+ */
+class TransitionerException extends AbstractBaseTransitionerException
+{
+    /**
+     * Constructor.
+     *
+     * @since [*next-version*]
+     *
+     * @param string|Stringable|null     $message      The error message, if any.
+     * @param int|null                   $code         The error code, if any.
+     * @param RootException|null         $previous     The previous exception for chaining, if any.
+     * @param TransitionerInterface|null $transitioner The transitioner, if any.
+     */
+    public function __construct(
+        $message = null,
+        $code = null,
+        $previous = null,
+        TransitionerInterface $transitioner = null
+    ) {
+        $this->_init($message, $code, $previous, $transitioner);
+    }
+}
