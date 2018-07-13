eb0dd12d52c2f137a9412b36b1ed438ec7fc397b
Miguel Muscat <miguelmuscat93@gmail.com>
2018-07-13 18:10:58 +0200

    Exceptions now use awareness traits

--- src/Exception/CouldNotTransitionException.php
+++ src/Exception/CouldNotTransitionException.php
@@ -2,8 +2,10 @@
 
 namespace Dhii\Data\Exception;
 
+use Dhii\Data\StateAwareAwareTrait;
 use Dhii\Data\StateAwareInterface;
 use Dhii\Data\StateAwareTrait;
+use Dhii\Data\TransitionAwareTrait;
 use Dhii\Data\TransitionerInterface;
 use Dhii\Util\Normalization\NormalizeStringableCapableTrait;
 use Dhii\Util\String\StringableInterface as Stringable;
@@ -19,25 +21,18 @@ class CouldNotTransitionException extends AbstractBaseTransitionerException impl
     CouldNotTransitionExceptionInterface
 {
     /* @since [*next-version*] */
+    use TransitionAwareTrait {
+        _getTransition as public getTransition;
+    }
+
+    /* @since [*next-version*] */
+    use StateAwareAwareTrait {
+        _getStateAware as public getSubject;
+        _setStateAware as _setSubject;
+    }
+
+    /* @since [*next-version*] */
     use NormalizeStringableCapableTrait;
-
-    /**
-     * The transition, if any.
-     *
-     * @since [*next-version*]
-     *
-     * @var string|Stringable|null
-     */
-    protected $transition;
-
-    /**
-     * The subject, if any.
-     *
-     * @since [*next-version*]
-     *
-     * @var StateAwareInterface|null
-     */
-    protected $subject;
 
     /**
      * Constructor.
@@ -65,61 +60,5 @@ class CouldNotTransitionException extends AbstractBaseTransitionerException impl
         $this->_setTransitioner($transitioner);
         $this->_setTransition($transition);
         $this->_setSubject($subject);
-    }
-
-    /**
-     * {@inheritdoc}
-     *
-     * @since [*next-version*]
-     */
-    public function getTransition()
-    {
-        return $this->transition;
-    }
-
-    /**
-     * Sets the transition.
-     *
-     * @since [*next-version*]
-     *
-     * @param string|Stringable|null $transition The transition, if any.
-     *
-     * @throws InvalidArgumentException If the argument is not a string or stringable object.
-     */
-    protected function _setTransition($transition)
-    {
-        $this->transition = ($transition !== null)
-            ? $this->_normalizeStringable($transition)
-            : null;
-    }
-
-    /**
-     * {@inheritdoc}
-     *
-     * @since [*next-version*]
-     */
-    public function getSubject()
-    {
-        return $this->subject;
-    }
-
-    /**
-     * Sets the subject.
-     *
-     * @since [*next-version*]
-     *
-     * @param StateAwareTrait|null $subject The subject, if any.
-     *
-     * @throws InvalidArgumentException If the argument is not a state-aware object.
-     */
-    protected function _setSubject($subject)
-    {
-        if ($subject !== null && !($subject instanceof StateAwareInterface)) {
-            throw $this->_createInvalidArgumentException(
-                $this->__('Argument is not a state-aware object'), null, null, $subject
-            );
-        }
-
-        $this->subject = $subject;
     }
 }
